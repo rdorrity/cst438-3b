1b4570bae700f412fde90759e141730dc5ca3bbc
Ryan <rdorrity@gmail.com>
2020-03-24 20:16:35 -0700

    Renamed Project and loaded to new repo

---
+++ src/test/java/com/example/cst4383Reservation/Cst4383ReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cst4383Reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst4383ReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
